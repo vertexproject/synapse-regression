82583c5c8c12a82b69d2f68f9f38913a1c643b69
blackout <blackout@vertex.link>
2023-11-03 11:51:47 -0400

    wip

--- scripts/gen-0.2.22.py
+++ scripts/gen-0.2.22.py
@@ -15,7 +15,10 @@ async def main():
 
     async with await s_cortex.Cortex.anit(tmpdir) as core:
 
+        # 100.64.0.0/10 goes from 100.64.0.0 to 100.127.255.255. Get a good chunk at the beginning
+        # and the broadcast address for bookend testing.
         await core.nodes('[ inet:ipv4=100.64.0.0/24 ]')
+        await core.nodes('[ inet:ipv4=100.127.255.255 ]')
 
     s_backup.backup(tmpdir, modldir)
 
